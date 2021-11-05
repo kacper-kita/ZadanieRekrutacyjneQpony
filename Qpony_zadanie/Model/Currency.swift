0c4a2fe84567b8d184d3c2cba199f844a4f4e00b
kacper-kita <kkita.contact@gmail.com>
2021-11-05 10:16:53 +0100

    Add optional mark to model values

--- Qpony_zadanie/Model/Currency.swift
+++ Qpony_zadanie/Model/Currency.swift
@@ -8,14 +8,14 @@
 import Foundation
 
 struct Currencies: Decodable {
-    let currency: String
-    let code: String
-    let mid: Double
+    let currency: String?
+    let code: String?
+    let mid: Double?
 }
 
 struct Currency: Decodable {
-    let table: String
-    let no: String
-    let effectiveDate: Date
-    let rates: [Currencies]
+    let table: String?
+    let no: String?
+    let effectiveDate: Date?
+    let rates: [Currencies]?
 }
