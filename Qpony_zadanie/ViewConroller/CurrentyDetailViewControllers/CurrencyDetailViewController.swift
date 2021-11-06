70c21da70235917bc1e7cb6220b48fbb7626cf66
kacper-kita <kkita.contact@gmail.com>
2021-11-06 16:16:14 +0100

    Create CurrencyDetailScreen layout

--- Qpony_zadanie/ViewConroller/CurrentyDetailViewControllers/CurrencyDetailViewController.swift
+++ Qpony_zadanie/ViewConroller/CurrentyDetailViewControllers/CurrencyDetailViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class CurrencyDetailViewController: UIViewController {
     
+    //MARK: - Outlets
+    
+    @IBOutlet weak var startDate: UIDatePicker!
+    @IBOutlet weak var endDate: UIDatePicker!
+    @IBOutlet weak var tableView: UITableView!
+    
     var name = ""
     var code = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("DetailData: \(name)")
+        self.title = name
     }
+    
+    //MARK: - Actions
+    
+    @IBAction func didTapReload(_ sender: UIButton) {
+    }
+
+    @IBAction func didTapOK(_ sender: UIButton) {
+    }
+}
+
+extension CurrencyDetailViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    } 
 }
