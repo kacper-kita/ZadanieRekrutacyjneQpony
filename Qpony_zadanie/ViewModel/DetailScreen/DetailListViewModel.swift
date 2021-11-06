b6867ffee2210fe5f6ef4bce8db247372f39348c
kacper-kita <kkita.contact@gmail.com>
2021-11-06 16:28:34 +0100

    Create CurrencyDetail ViewModels

---
+++ Qpony_zadanie/ViewModel/DetailScreen/DetailListViewModel.swift
@@ -0,0 +1,26 @@
+//
+//  DetailListViewModel.swift
+//  Qpony_zadanie
+//
+//  Created by Kacper Kita on 06/11/2021.
+//
+
+import Foundation
+
+class DetailListViewModel {
+    var detailVM = [DetailViewModel]()
+    
+    func getDetails(table: String, code: String, startDate: String, endDate: String, completion: @escaping ([DetailViewModel]) -> Void) {
+        NetworkManager.shared.getCurrencyDetail(table: table, code: code, startDate: startDate, endDate: endDate) { details in
+            guard let details = details else {
+                return
+            }
+            
+            let detailVM = details.map(DetailViewModel.init)
+            DispatchQueue.main.async {
+                self.detailVM = detailVM
+                completion(detailVM)
+            }
+        }
+    }
+}
---
+++ Qpony_zadanie/ViewModel/DetailScreen/DetailViewModel.swift
@@ -0,0 +1,24 @@
+//
+//  DetailViewModel.swift
+//  Qpony_zadanie
+//
+//  Created by Kacper Kita on 06/11/2021.
+//
+
+import Foundation
+
+struct DetailViewModel {
+    let details: CurrencyRates
+    
+    var no: String {
+        return details.no ?? "Unknown"
+    }
+    
+    var effectiveDate: String {
+        return details.effectiveDate ?? "Unknown"
+    }
+    
+    var mid: Double {
+        return details.mid ?? 0.0
+    }
+}
